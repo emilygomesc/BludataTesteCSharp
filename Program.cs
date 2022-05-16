0bbc6684c065762aaf8c79a796e74feeaaa9cba9
emilygomesc <89324438+emilygomesc@users.noreply.github.com>
2022-05-16 11:36:15 -0300

    New  Final Commit with lost archives

---
+++ Program.cs
@@ -0,0 +1,21 @@
+using BludataAPI;
+using BludataAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace BludataAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
